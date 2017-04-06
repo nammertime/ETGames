237104584cee3ae80625d0aa853454d80901e85d
Ubuntu <vagrant@Base-Box>
2017-04-06 18:39:42 +0000

    had problem with session_stored had to set forgery to check nulls

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ETGames_session',
-:key => '_my_session',
- :expire_after => 30.seconds
+Rails.application.config.session_store :cookie_store, key: '_ETGames_session'
