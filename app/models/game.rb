8878d8c1b238818e028c7bf89dec1a29216f9eab
Ubuntu <vagrant@Base-Box>
2017-04-13 19:23:44 +0000

    added comments to erbs... getting desperate for commits...

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,9 @@
 class Game < ApplicationRecord
 
-
+ validates :ProductName, presence: true
+ validates :ProductPrice, presence: true
+ validates :ProductStock, presence: true
+ validates :ProductDesc, presence: true
+ validates :product_genres, presence: true
 
 end
